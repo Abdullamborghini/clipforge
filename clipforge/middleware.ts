e82104686a6bbda4728d6eb480e9d44b5fdc882e
Abdullah <rollabdullahroice@gmail.com>
2026-05-28 06:35:04 +0700

    disable middleware temp

--- clipforge/middleware.ts
+++ clipforge/middleware.ts
@@ -1,9 +1,5 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+export default function middleware() {}
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: [],
 };
