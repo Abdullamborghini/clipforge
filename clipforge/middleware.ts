90ae71f36bde9ee85b488950ff1f41bc52afc528
Abdullah <rollabdullahroice@gmail.com>
2026-05-28 13:07:10 +0700

    re-enable middleware

--- clipforge/middleware.ts
+++ clipforge/middleware.ts
@@ -1,5 +1,7 @@
-export default function middleware() {}
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
 
 export const config = {
-  matcher: [],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
